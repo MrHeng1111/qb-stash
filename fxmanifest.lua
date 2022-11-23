1ba3df5b037c7429e109584e25274a2c27321015
MrHeng1111 <92356141+MrHeng1111@users.noreply.github.com>
2022-11-23 14:04:23 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'MKRP-Devaloper'
+description 'qb-stashs for QBCore Created By MKRP Devaloper'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+	'client/main.lua',
+	'client/menus.lua'
+}
